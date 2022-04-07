cce982412c189984eae16e50de01d5bb1c7be89e
shabialili <sebnem.eliyeva1998@gmail.com>
2022-04-07 16:06:28 +0400

    Proxy Pattern

---
+++ ProxyPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proxy result = new Proxy();
+            result.Sum(10, 5);
+            result.Sub(10, 5);
+            result.Div(10, 5);
+            result.Multi(10, 5);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ProxyPattern/ProxyPattern.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    public interface ISubject
+    {
+        void Sum(int x, int y);
+        void Div(int x, int y);
+        void Multi(int x, int y);   
+        void Sub(int x, int y); 
+    }
+    public class RealSubject : ISubject
+    {
+        public void Sum(int x, int y)
+        {
+            Console.WriteLine("x+y = {2} ",x,y, x+y);        
+        }
+
+        public void Div(int x, int y)
+        {
+            Console.WriteLine("x/y = {2}",x,y, x/y);
+        }
+
+        public void Multi(int x, int y)
+        {
+            Console.WriteLine("x*y = {2}",x,y, x*y);
+        }
+
+        public void Sub(int x, int y)
+        {
+            Console.WriteLine("x-y = {2}",x,y, x - y);
+        }
+    }
+    public class Proxy
+    {
+        private RealSubject real = new RealSubject();
+        public void Sum(int x, int y)
+        {
+            Console.WriteLine();
+            real.Sum(x, y);
+        }
+
+        public void Div(int x, int y)
+        {
+            Console.WriteLine();
+            real.Div(x, y);
+        }
+
+        public void Multi(int x, int y)
+        {
+            Console.WriteLine();
+            real.Multi(x, y);
+        }
+
+        public void Sub(int x, int y)
+        {
+            Console.WriteLine();
+            real.Sub(x, y);
+        }
+    }
+}
